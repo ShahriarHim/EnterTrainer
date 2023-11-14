9d5de7b2857a5bf4141d12e5e013314eae896184
ShahriarHim <shahriarhim19@gmail.com>
2023-11-14 19:43:29 +0600

    Added Navbar

--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/home";
 import LoginPage from "../pages/signin";
 import SignupPage from "../pages/signup";
+import Navbar from "../pages/Components/Navbar";
 const Index = () => {
   return (
     <div>
@@ -10,6 +11,7 @@ const Index = () => {
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/signup" element={<SignupPage />}></Route>
+        <Route path ="/nab" element={<Navbar />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
