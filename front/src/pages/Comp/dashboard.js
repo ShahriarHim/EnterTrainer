04d0e2069ddac3e0ee0ffb3e89bbcdfbeb273bb9
ShahriarHim <shahriarhim19@gmail.com>
2023-11-20 06:51:50 +0600

    done with Module 1

--- front/src/pages/Comp/dashboard.js
+++ front/src/pages/Comp/dashboard.js
@@ -33,7 +33,7 @@ const Dashboard = () => {
           <Link to="/courses" className="nav-item nav-link">
             Courses
           </Link>
-          <Link to="/contact" className="nav-item nav-link">
+          <Link to="/profile" className="nav-item nav-link">
             Profile
           </Link>
         </div>
