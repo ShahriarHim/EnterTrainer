46c03bbdc600d1d9b18d248f9d7fd2436f924ed1
Ismail <ismail.hossain.saihan@g.bracu.ac.bd>
2023-11-20 06:19:40 +0600

    Profile added

---
+++ front/src/pages/Profile/profile.js
@@ -0,0 +1,43 @@
+// Profile.jsx
+
+import React from 'react';
+import GeneralSettings from './GeneralSettings';
+import ChangePassword from './ChangePassword';
+import AccountInfo from './AccountInfo';
+import SocialLinks from './SocialLinks';
+import '../../assets/css/profile.css';
+
+
+const Profile = () => {
+    return (
+      <div className="container light-style flex-grow-1 container-p-y">
+        <h4 className="font-weight-bold py-3 mb-4">Account settings</h4>
+        <div className="card overflow-hidden">
+          <div className="row no-gutters row-bordered row-border-light">
+            <div className="col-md-3 pt-0">
+              <div className="list-group list-group-flush account-settings-links">
+                <a className="list-group-item list-group-item-action active" data-toggle="list" href="#account-general">General</a>
+                <a className="list-group-item list-group-item-action" data-toggle="list" href="#account-change-password">Change password</a>
+                <a className="list-group-item list-group-item-action" data-toggle="list" href="#account-info">Info</a>
+                <a className="list-group-item list-group-item-action" data-toggle="list" href="#account-social-links">Social links</a>
+              </div>
+            </div>
+            <div className="col-md-9">
+              <div className="tab-content">
+                <GeneralSettings />
+                <ChangePassword />
+                <AccountInfo />
+                <SocialLinks />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-right mt-3">
+          <button type="button" className="btn btn-primary">Save changes</button>&nbsp;
+          <button type="button" className="btn btn-default">Cancel</button>
+        </div>
+      </div>
+    );
+  };
+
+export default Profile;
--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -1,21 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/home";
-
 import JoinForm from "../pages/Comp/joinform";
 import IndexPage from "../pages/indexPage";
 import ForgotPass from "../pages/ResetPass/forgotPass";
 import ResetPass from "../pages/ResetPass/resetPass";
+import Profile from "../pages/Profile/profile";
+
+
 const Index = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-        
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/home" element={<IndexPage />}></Route>
         <Route path="/joinform" element={<JoinForm />}></Route>
         <Route path="/forgot-password" element={<ForgotPass />}></Route>
         <Route path="/reset-password" element={<ResetPass />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
