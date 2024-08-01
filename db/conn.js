3902f6a6d98fc009b0fc74f3fad1dd2ece8d0d8c
Shahriar Bin Zaman Him <shahriarhim19@gmail.com>
2024-08-02 02:06:10 +0600

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
-const DB = process.env.DATABASE;
+// const DB = process.env.DATABASE;
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
 
+const DB = `mongodb+srv://${username}:${password}@cluster0.vkpqcd1.mongodb.net/?retryWrites=true&w=majority`;
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
