4feef97676667c87bbeaf83fe71ae36c10384704
Shahriar Bin Zaman Him <shahriarhim19@gmail.com>
2024-08-02 00:58:29 +0600

    Deploy 1

--- src/pages/Comp/footer.js
+++ src/pages/Comp/footer.js
@@ -132,7 +132,7 @@ const Footer = () => {
             <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
               &copy;{" "}
               <a className="border-bottom" href="#">
-                CSE471
+                Entertrainer
               </a>
               , All Right Reserved.
               {/*/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/}
